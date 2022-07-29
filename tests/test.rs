de5e60329f72da686a3c3d9f0e13e59d12873f8b
Matthew Hammer <matthew.hammer@gmail.com>
2022-07-29 11:04:05 -0600

    more tests.

--- tests/test.rs
+++ tests/test.rs
@@ -1,9 +1,36 @@
 use motoko::check::assert_roundtrip;
 
 #[test]
-fn test_numbers() {
+fn test_chars() {
+    assert_roundtrip("'h'");
+}
+
+#[test]
+fn test_strings() {
+    assert_roundtrip("\"hello\"");
+}
+
+#[test]
+fn test_unit() {
+    assert_roundtrip("()");
+}
+
+#[test]
+fn test_nats() {
     assert_roundtrip("0");
     assert_roundtrip("123");
+}
+
+#[test]
+fn test_ints() {
+    assert_roundtrip("-0");
+    assert_roundtrip("-123");
+}
+
+#[test]
+fn test_floats() {
+    assert_roundtrip("-0.0");
+    assert_roundtrip("-123.123");
 }
 
 #[test]

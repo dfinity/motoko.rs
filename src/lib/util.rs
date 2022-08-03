87f0bc3a5dc6d34415c5be8180cebbda11ce2f2f
Matthew Hammer <matthew.hammer@gmail.com>
2022-08-03 12:11:23 -0600

    parser follows Motoko compiler's design, uses higher-order productions.

--- src/lib/util.rs
+++ src/lib/util.rs
@@ -1,9 +1,16 @@
-use crate::ast::Delim;
+use crate::ast::{Dec, Exp, Delim};
 
 pub fn get_one<T>(d: Delim<T>) -> Result<Box<T>, Delim<T>> {
     if d.vec.len() == 1 && !d.has_trailing {
         Ok(Box::new(d.vec.into_iter().nth(0).unwrap()))
     } else {
         Err(d)
+    }
+}
+
+pub fn dec_into_exp(d: Dec) -> Exp {
+    match d {
+        Dec::Exp(e) => e,
+        d => Exp::Block(Delim{vec:vec!(d), has_trailing: false}),
     }
 }

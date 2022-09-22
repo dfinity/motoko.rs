542e1a7b8aa3117ee03c8c82ddeeae43abee684d
Matthew Hammer <matthew.hammer@gmail.com>
2022-09-22 14:05:30 -0600

    needs its own crate

--- src/lib/ppx.rs
+++ src/lib/ppx.rs
@@ -1,14 +1,14 @@
 extern crate proc_macro;
 use proc_macro::TokenStream;
 
-use crate::check::parse;
+use crate::check::parse as parse_;
 
 // https://doc.rust-lang.org/reference/procedural-macros.html
 
 #[proc_macro]
 pub fn parse(stream: TokenStream) -> TokenStream {
 
-    
+    todo!()
 
     // "fn answer() -> u32 { 42 }".parse().unwrap()
 }

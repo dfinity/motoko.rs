4d7ce552fed9f67b9c9393ee64c66fed0bb4dc1e
Ryan Vandersmith <ryan.vandersmith@dfinity.org>
2022-09-23 15:09:06 -0600

    Refactor for 'parse_static!' and 'eval_static!'

--- crates/motoko_proc_macro/src/lib.rs
+++ crates/motoko_proc_macro/src/lib.rs
@@ -1,20 +1,60 @@
-extern crate lazy_static;
 extern crate proc_macro;
 
 use proc_macro::TokenStream;
 use quote::quote;
 
-// https://doc.rust-lang.org/reference/procedural-macros.html
+#[proc_macro]
+pub fn parse_static(input: TokenStream) -> TokenStream {
+    let input = get_input_string(input);
+    expand_static(
+        &motoko::check::parse(&input).expect("Parse error"),
+        "::motoko::ast::Prog",
+    )
+}
 
 #[proc_macro]
-pub fn parse_static(stream: TokenStream) -> TokenStream {
-    let input = syn::parse::<syn::LitStr>(stream)
-        .expect("Unexpected macro input")
-        .value();
-    let prog = motoko::check::parse(&input).expect("Evaluation error");
-    let serialized = motoko::proc_macro::serialize(&prog).expect("Serialization error");
+pub fn eval_static(input: TokenStream) -> TokenStream {
+    let input = get_input_string(input);
+    expand_static(
+        &motoko::vm::eval(&input).expect("Evaluation error"),
+        "::motoko::value::Value",
+    )
+}
+
+fn expand_static<'de, T: Sized + serde::Serialize + serde::Deserialize<'de>>(
+    value: &T,
+    type_path: &'static str,
+) -> TokenStream {
+    let serialized = motoko::proc_macro::serialize(value).expect("Serialization error");
     let serialized_ast = proc_macro2::Literal::byte_string(&serialized);
+    let typ = syn::Type::Verbatim(type_path.parse().expect("Type parse error"));
+
     TokenStream::from(quote! {{
-        motoko::proc_macro::deserialize::<motoko::ast::Prog>(#serialized_ast as &[u8]).unwrap()
+        // Derived from the `lazy_static!` macro expansion
+        struct STATIC;
+        impl std::ops::Deref for STATIC {
+            type Target = #typ;
+            fn deref(&self) -> &#typ {
+                struct _AssertSync where #typ: std::marker::Sync;
+                struct _AssertSized where #typ: std::marker::Sized;
+
+                static ONCE: std::sync::Once = std::sync::Once::new();
+                static mut VALUE: *mut #typ = 0 as *mut #typ;
+
+                unsafe {
+                    ONCE.call_once(|| VALUE = Box::into_raw(Box::new(
+                        ::motoko::proc_macro::deserialize::<#typ>(#serialized_ast).expect("Deserialization error")
+                    )));
+                    &*VALUE
+                }
+            }
+        };
+        &*STATIC
     }})
+}
+
+fn get_input_string(input: TokenStream) -> String {
+    syn::parse::<syn::LitStr>(input)
+        .expect("Unexpected macro input")
+        .value()
 }

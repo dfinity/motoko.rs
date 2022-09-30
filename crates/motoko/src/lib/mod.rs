0a5ba1c3ec2885022c4ec3b9631c64b80497eaae
Matthew Hammer <matthew.hammer@gmail.com>
2022-09-30 15:18:04 -0600

    focus on VM type errors.

--- crates/motoko/src/lib/mod.rs
+++ crates/motoko/src/lib/mod.rs
@@ -1,27 +1,27 @@
 pub mod ast;
-pub mod ast_traversal;
-#[cfg(feature = "parser")]
-pub mod check;
-pub mod convert;
+//pub mod ast_traversal;
+//#[cfg(feature = "parser")]
+//pub mod check;
+//pub mod convert;
 pub mod dynamic;
-#[cfg(feature = "parser")]
-pub mod format;
-#[cfg(feature = "parser")]
-mod format_utils;
-#[cfg(feature = "parser")]
-pub mod lexer;
-#[cfg(feature = "parser")]
-pub mod lexer_types;
-pub mod package;
-#[allow(clippy::all)]
-#[cfg(feature = "parser")]
-pub mod parser;
-#[cfg(feature = "parser")]
-pub mod parser_types;
-#[cfg(feature = "parser")]
-mod parser_utils;
-#[doc(hidden)]
-pub mod proc_macro;
+//#[cfg(feature = "parser")]
+//pub mod format;
+//#[cfg(feature = "parser")]
+//mod format_utils;
+//#[cfg(feature = "parser")]
+//pub mod lexer;
+//#[cfg(feature = "parser")]
+//pub mod lexer_types;
+//pub mod package;
+//#[allow(clippy::all)]
+//#[cfg(feature = "parser")]
+//pub mod parser;
+//#[cfg(feature = "parser")]
+//pub mod parser_types;
+//#[cfg(feature = "parser")]
+//mod parser_utils;
+//#[doc(hidden)]
+//pub mod proc_macro;
 mod serde_utils;
 pub mod value;
 pub mod vm;

71d88b7ca93348696d029006eba5a09e16bb4000
rvanasa <ryan.vandersmith@dfinity.org>
2022-11-23 14:18:03 -0700

    Add base library import test

--- crates/motoko/tests/test_packages.rs
+++ crates/motoko/tests/test_packages.rs
@@ -1,10 +1,22 @@
-// use motoko::package::get_base_library;
+use motoko::{package::get_base_library, vm_types::Core};
+
+use test_log::test;
 
 #[test]
 fn base_library() {
-    // let base = get_base_library();
+    let base = get_base_library();
+    assert!(base.files.len() > 10);
 
-    // assert!(base.files.len() > 10);
-
-    assert_eq!(1, 1); // temp
+    let mut core = Core::empty();
+    let mut success = true;
+    for (path, file) in base.files {
+        match core.set_module(path.clone(), &file.content) {
+            Ok(_) => println!("✅ {}", path),
+            Err(i) => {
+                success = false;
+                println!("❌ {} : {:?}", path, i)
+            }
+        }
+    }
+    assert!(success);
 }

933d1c78189251edb6de300f81054d325b68c842
rvanasa <ryan.vandersmith@dfinity.org>
2022-11-23 14:23:53 -0700

    Sort base library files in unit test

--- crates/motoko/tests/test_packages.rs
+++ crates/motoko/tests/test_packages.rs
@@ -8,8 +8,10 @@ fn base_library() {
     assert!(base.files.len() > 10);
 
     let mut core = Core::empty();
+    let mut files = base.files.into_iter().collect::<Vec<_>>();
+    files.sort_by_cached_key(|(path, _)| path.clone());
     let mut success = true;
-    for (path, file) in base.files {
+    for (path, file) in files {
         match core.set_module(path.clone(), &file.content) {
             Ok(_) => println!("✅ {}", path),
             Err(i) => {

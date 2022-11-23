c939adeb749f1ecd51c1282026453daf9c1a1e55
rvanasa <ryan.vandersmith@dfinity.org>
2022-11-23 14:26:03 -0700

    Only parse base library for now

--- crates/motoko/tests/test_packages.rs
+++ crates/motoko/tests/test_packages.rs
@@ -1,18 +1,17 @@
-use motoko::{package::get_base_library, vm_types::Core};
+use motoko::{package::get_base_library};
 
 use test_log::test;
 
 #[test]
-fn base_library() {
+fn parse_base_library() {
     let base = get_base_library();
     assert!(base.files.len() > 10);
 
-    let mut core = Core::empty();
     let mut files = base.files.into_iter().collect::<Vec<_>>();
     files.sort_by_cached_key(|(path, _)| path.clone());
     let mut success = true;
     for (path, file) in files {
-        match core.set_module(path.clone(), &file.content) {
+        match motoko::check::parse(&file.content) {
             Ok(_) => println!("✅ {}", path),
             Err(i) => {
                 success = false;
@@ -22,3 +21,24 @@ fn base_library() {
     }
     assert!(success);
 }
+
+// #[test]
+// fn load_base_library() {
+//     let base = get_base_library();
+//     assert!(base.files.len() > 10);
+
+//     let mut core = Core::empty();
+//     let mut files = base.files.into_iter().collect::<Vec<_>>();
+//     files.sort_by_cached_key(|(path, _)| path.clone());
+//     let mut success = true;
+//     for (path, file) in files {
+//         match core.set_module(path.clone(), &file.content) {
+//             Ok(_) => println!("✅ {}", path),
+//             Err(i) => {
+//                 success = false;
+//                 println!("❌ {} : {:?}", path, i)
+//             }
+//         }
+//     }
+//     assert!(success);
+// }

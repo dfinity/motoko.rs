d4f22517caafe40566fd26cbcaca1b676d063796
rvanasa <ryan.vandersmith@dfinity.org>
2022-12-01 11:33:55 -0700

    Add 'eval_base_library' and 'eval_base_library_tests'

--- crates/motoko/tests/test_packages.rs
+++ crates/motoko/tests/test_packages.rs
@@ -1,10 +1,13 @@
-use motoko::package::{get_base_library, get_base_library_tests, Package};
+use motoko::{
+    package::{get_base_library, get_base_library_tests, Package},
+    vm_types::Core,
+};
 
 use test_log::test;
 
 fn assert_parse_package(package: Package) {
     println!("Parsing package: {}", package.name);
-    assert!(package.files.len() > 0);
+    assert!(!package.files.is_empty());
 
     let mut files = package.files.into_iter().collect::<Vec<_>>();
     files.sort_by_cached_key(|(path, _)| path.clone());
@@ -30,6 +33,36 @@ fn assert_parse_package(package: Package) {
     assert_eq!(total - error_count, total);
 }
 
+fn assert_eval_package(package: Package) {
+    println!("Evaluating package: {}", package.name);
+    assert!(!package.files.is_empty());
+
+    let mut files = package.files.into_iter().collect::<Vec<_>>();
+    files.sort_by_cached_key(|(path, _)| path.clone());
+    let total = files.len();
+    let mut count = 0;
+    let mut error_count = 0;
+    for (path, file) in files {
+        count += 1;
+        let mut core = Core::empty();
+        match core.eval(&file.content) {
+            Ok(_) => println!(" {}. ✅ {}", count, path),
+            Err(i) => {
+                error_count += 1;
+                println!(" {}. ❌ {} : {:?}", count, path, i)
+            }
+        }
+    }
+    println!("Attempted to evaluate {} modules.", total);
+    if error_count > 0 {
+        println!("  But, found {} modules did not load.", error_count);
+    } else {
+        println!("  Success!");
+    }
+
+    assert!(false);
+}
+
 #[test]
 fn parse_base_library() {
     assert_parse_package(get_base_library());
@@ -40,23 +73,12 @@ fn parse_base_library_tests() {
     assert_parse_package(get_base_library_tests())
 }
 
-// #[test]
-// fn load_base_library() {
-//     let base = get_base_library();
-//     assert!(base.files.len() > 10);
+#[test]
+fn eval_base_library() {
+    assert_eval_package(get_base_library());
+}
 
-//     let mut core = Core::empty();
-//     let mut files = base.files.into_iter().collect::<Vec<_>>();
-//     files.sort_by_cached_key(|(path, _)| path.clone());
-//     let mut success = true;
-//     for (path, file) in files {
-//         match core.set_module(path.clone(), &file.content) {
-//             Ok(_) => println!("✅ {}", path),
-//             Err(i) => {
-//                 success = false;
-//                 println!("❌ {} : {:?}", path, i)
-//             }
-//         }
-//     }
-//     assert!(success);
-// }
+#[test]
+fn eval_base_library_tests() {
+    assert_eval_package(get_base_library_tests());
+}

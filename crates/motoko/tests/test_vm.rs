52d61ae3884d54e2fc5c00e6fb61b77770ec6fa9
Matthew Hammer <matthew.hammer@gmail.com>
2022-12-28 10:31:27 -0700

    unit test for fix that is independent of base library tests.

--- crates/motoko/tests/test_vm.rs
+++ crates/motoko/tests/test_vm.rs
@@ -86,6 +86,8 @@ fn vm_vars() {
     assert_("var x = 1; x", "1");
     assert_("var x = 1; x := 2; x", "2");
     assert_x("1 := 1", &type_mismatch_!());
+    assert_("var x : Nat = 1", "()");
+    assert_("var (x : Nat) = 1", "()");
 }
 
 #[test]

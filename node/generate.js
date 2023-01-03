f851a1abeba591a91d7b1c50295faa907e575396
rvanasa <ryan.vandersmith@dfinity.org>
2023-01-03 10:34:20 -0700

    Preprocess 'prim.mo' file

--- node/generate.js
+++ node/generate.js
@@ -10,20 +10,25 @@ const packages = [
   { name: "matchers", path: "kritzcreek/motoko-matchers/master/src" },
 ];
 
+const primSource = readFileSync(
+  join(__dirname, "../submodules/motoko/src/prelude/prim.mo"),
+  "utf8"
+);
+
+writeFileSync(
+  join(__dirname, "../crates/motoko/src/packages/prim.mo"),
+  `module {\n${primSource
+    .replaceAll(/^func/gm, "public func")
+    .replaceAll(/^/gm, "  ")}\n}`,
+  "utf8"
+);
+
 Promise.all(
   packages.map(async ({ name, path }) => {
     const pkg = await mo.fetchPackage(name, path);
     writeFileSync(
       join(__dirname, `../crates/motoko/src/packages/${name}.json`),
       JSON.stringify(pkg),
-      "utf8"
-    );
-    writeFileSync(
-      join(__dirname, "../crates/motoko/src/packages/prim.mo"),
-      readFileSync(
-        join(__dirname, "../submodules/motoko/src/prelude/prim.mo"),
-        "utf8"
-      ),
       "utf8"
     );
   })

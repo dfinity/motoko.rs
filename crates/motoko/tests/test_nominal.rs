68eafa367721a28e8d181420c7fee6d6ee44b733
matthewhammer <matthew@dfinity.org>
2023-05-03 12:40:47 -0600

    thunk parse case. more test cases.

--- crates/motoko/tests/test_nominal.rs
+++ crates/motoko/tests/test_nominal.rs
@@ -10,6 +10,16 @@ fn sanity_1_plus_2_equals_3() {
 
 
 #[test]
+fn force_thunk_2() {
+    assert_("force (thunk 2)", "2")
+}
+
+#[test]
+fn memo_137_plus_137() {
+    assert_("memo (137 + 137)", "274")
+}
+
+#[test]
 fn get_put_4() {
     assert_("@(@1 := 4)", "4")
 }
